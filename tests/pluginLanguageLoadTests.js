3991cbd36ef015eeb75af808145b85e705a3ca8e
Max Nachlinger <max.nachlinger@gmail.com>
2014-09-06 22:13:50 -0700

    dev mode tests

--- tests/pluginLanguageLoadTests.js
+++ tests/pluginLanguageLoadTests.js
@@ -3,7 +3,7 @@ var path = require('path');
 var test = require('tape');
 var index = require('../');
 
-test('Include-exclude mode can be setup', function(t) {
+test('Returns lcale codes for all POs', function(t) {
 	var exposed = {};
 	var plugIn = {
 		log: function() {},
@@ -25,3 +25,27 @@ test('Include-exclude mode can be setup', function(t) {
 		t.end();
 	});
 });
+
+test('Returns the test locale when no PO files found', function(t) {
+	var exposed = {};
+	var plugIn = {
+		log: function() {},
+		state: function() {},
+		expose: function(key, value) {
+			exposed[key] = value;
+		},
+		ext: function() {}
+	};
+	var options = {
+		cookieName: '_locale',
+		l10nDirectory: path.resolve(__dirname, 'fixtures/empty'),
+		defaultLocale: 'en' // if the cookie is absent and no header is passed, this locale is used
+	};
+	index.register(plugIn, options, function() {
+		[ 'test' ].forEach(function(locale) {
+			t.ok(~exposed['localeCodes'].indexOf(locale), "Found " + locale);
+		});
+		t.end();
+	});
+});
+
